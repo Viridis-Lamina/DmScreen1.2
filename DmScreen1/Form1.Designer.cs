069eeb92e6fa8cb7791852d2ed74c1b9e8782122
Ryan Clarke <ryan@dogtoothtech.com>
2023-12-05 13:58:06 +0000

    Add project files.

---
+++ DmScreen1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace DmScreen1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Dicebox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Nonlabel1 = new System.Windows.Forms.Label();
+            this.Nonlabel2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Damagebox = new System.Windows.Forms.TextBox();
+            this.Nonlabel3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxAdvantage = new System.Windows.Forms.CheckBox();
+            this.checkBoxDisadvantage = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 156);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vairable data";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Dalelands Uncial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(780, 65);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Roll!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Dicebox
+            // 
+            this.Dicebox.Location = new System.Drawing.Point(175, 68);
+            this.Dicebox.Margin = new System.Windows.Forms.Padding(4);
+            this.Dicebox.Name = "Dicebox";
+            this.Dicebox.Size = new System.Drawing.Size(132, 22);
+            this.Dicebox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 421);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Roll Result";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40"});
+            this.comboBox1.Location = new System.Drawing.Point(377, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(107, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Nonlabel1
+            // 
+            this.Nonlabel1.AutoSize = true;
+            this.Nonlabel1.Font = new System.Drawing.Font("Dalelands Uncial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nonlabel1.Location = new System.Drawing.Point(5, 69);
+            this.Nonlabel1.Name = "Nonlabel1";
+            this.Nonlabel1.Size = new System.Drawing.Size(148, 21);
+            this.Nonlabel1.TabIndex = 5;
+            this.Nonlabel1.Text = "Roll to Hit!";
+            // 
+            // Nonlabel2
+            // 
+            this.Nonlabel2.AutoSize = true;
+            this.Nonlabel2.Font = new System.Drawing.Font("Dalelands Uncial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nonlabel2.Location = new System.Drawing.Point(325, 69);
+            this.Nonlabel2.Name = "Nonlabel2";
+            this.Nonlabel2.Size = new System.Drawing.Size(43, 21);
+            this.Nonlabel2.TabIndex = 6;
+            this.Nonlabel2.Text = "AC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(415, 421);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Hit or Miss";
+            // 
+            // Damagebox
+            // 
+            this.Damagebox.Location = new System.Drawing.Point(641, 68);
+            this.Damagebox.Name = "Damagebox";
+            this.Damagebox.Size = new System.Drawing.Size(132, 22);
+            this.Damagebox.TabIndex = 8;
+            // 
+            // Nonlabel3
+            // 
+            this.Nonlabel3.AutoSize = true;
+            this.Nonlabel3.Font = new System.Drawing.Font("Dalelands Uncial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nonlabel3.Location = new System.Drawing.Point(513, 67);
+            this.Nonlabel3.Name = "Nonlabel3";
+            this.Nonlabel3.Size = new System.Drawing.Size(107, 21);
+            this.Nonlabel3.TabIndex = 9;
+            this.Nonlabel3.Text = "Damage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(677, 421);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Damage";
+            // 
+            // checkBoxAdvantage
+            // 
+            this.checkBoxAdvantage.AutoSize = true;
+            this.checkBoxAdvantage.Location = new System.Drawing.Point(896, 73);
+            this.checkBoxAdvantage.Name = "checkBoxAdvantage";
+            this.checkBoxAdvantage.Size = new System.Drawing.Size(95, 20);
+            this.checkBoxAdvantage.TabIndex = 11;
+            this.checkBoxAdvantage.Text = "Advantage";
+            this.checkBoxAdvantage.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDisadvantage
+            // 
+            this.checkBoxDisadvantage.AutoSize = true;
+            this.checkBoxDisadvantage.Location = new System.Drawing.Point(997, 73);
+            this.checkBoxDisadvantage.Name = "checkBoxDisadvantage";
+            this.checkBoxDisadvantage.Size = new System.Drawing.Size(114, 20);
+            this.checkBoxDisadvantage.TabIndex = 12;
+            this.checkBoxDisadvantage.Text = "Disadvantage";
+            this.checkBoxDisadvantage.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1357, 671);
+            this.Controls.Add(this.checkBoxDisadvantage);
+            this.Controls.Add(this.checkBoxAdvantage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Nonlabel3);
+            this.Controls.Add(this.Damagebox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Nonlabel2);
+            this.Controls.Add(this.Nonlabel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Dicebox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "DM screen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Dicebox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Nonlabel1;
+        private System.Windows.Forms.Label Nonlabel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Damagebox;
+        private System.Windows.Forms.Label Nonlabel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxAdvantage;
+        private System.Windows.Forms.CheckBox checkBoxDisadvantage;
+    }
+}
+
---
+++ DmScreen1/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DmScreen1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Random random = new Random(); // Declare Random instance outside methods
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dicestring = Dicebox.Text;
+
+            // New code using the flexible dice rolling mechanism for attack roll
+            Tuple<int, string> attackRollResult = RollDice(dicestring);
+
+            int diceresult = attackRollResult.Item1;
+            string diceRollDetails = attackRollResult.Item2;
+
+            int AC = comboBox1.SelectedItem?.ToString() != null ? int.Parse(comboBox1.SelectedItem.ToString()) : 0;
+            int damageresult = 0;
+
+            Random random = new Random();
+
+            bool advantage = checkBoxAdvantage.Checked;
+            bool disadvantage = checkBoxDisadvantage.Checked;
+
+            // Check if both advantage and disadvantage are selected
+            if (advantage && disadvantage)
+            {
+                MessageBox.Show("Cannot select both Advantage and Disadvantage.");
+                return;
+            }
+
+            // Display the details of the attack roll
+            label1.Text = $"Attack Roll: {diceRollDetails}\nEnemy AC: {AC}\n";
+
+            int roll1 = random.Next(1, 21);
+            int roll2 = random.Next(1, 21);
+
+            if (advantage)
+            {
+                diceresult = Math.Max(roll1, roll2);
+                label1.Text += $"\nRoll 1: {roll1} \nRoll 2: {roll2}";
+            }
+            else if (disadvantage)
+            {
+                diceresult = Math.Min(roll1, roll2);
+                label1.Text += $"\nRoll 1: {roll1} \nRoll 2: {roll2}";
+            }
+            else
+            {
+                diceresult = roll1;
+            }
+
+            // Handle damage roll
+            string damagestring = Damagebox.Text;
+
+            // New code using the flexible dice rolling mechanism for damage roll
+            Tuple<int, string> damageRollResult = RollDice(damagestring);
+
+            damageresult = damageRollResult.Item1;
+            string damageRollDetails = damageRollResult.Item2;
+
+            // Display the details of the damage roll
+            label4.Text = $"Damage Roll: {damageRollDetails}\n";
+
+            // Check for critical hit
+            if (diceresult == 20)
+            {
+                damageresult *= 2;
+                label4.Text += $"Critical hit! You deal {damageresult} damage!";
+            }
+            else
+            {
+                label4.Text += $"You deal {damageresult} damage!";
+            }
+
+            // Display the result of the attack
+            if (diceresult > AC)
+            {
+                label3.Text = "It's a Hit";
+            }
+            else
+            {
+                label3.Text = "It's a miss!";
+            }
+        }
+
+        private Tuple<int, string> RollDice(string input)
+        {
+            int total = 0;
+            List<string> rolls = new List<string>();
+
+            // Use regular expression to extract dice and constants
+            string pattern = @"(\d+d\d+|\d+)";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                string matchValue = match.Value;
+
+                if (matchValue.Contains("d"))
+                {
+                    // It's a dice expression like NdM
+                    Tuple<int, string> diceRollResult = RollSingleDice(matchValue);
+                    total += diceRollResult.Item1;
+
+                    rolls.Add($"{matchValue} ({diceRollResult.Item2})");
+                }
+                else
+                {
+                    // It's a constant
+                    total += int.Parse(matchValue);
+                    rolls.Add($"{matchValue}");
+                }
+            }
+
+            string calculation = string.Join(" + ", rolls);
+            return Tuple.Create(total, calculation);
+        }
+
+        private Tuple<int, string> RollSingleDice(string diceExpression)
+        {
+            string[] parts = diceExpression.Split('d');
+            int count = int.Parse(parts[0]);
+            int type = int.Parse(parts[1]);
+
+            Random random = new Random();
+            List<int> individualRolls = new List<int>();
+            int result = Enumerable.Range(1, count).Sum(_ =>
+            {
+                int roll = random.Next(1, type + 1);
+                individualRolls.Add(roll);
+                return roll;
+            });
+
+            string rollsText = $"{string.Join(", ", individualRolls)}";
+            return Tuple.Create(result, rollsText);
+        }
+    }
+}
